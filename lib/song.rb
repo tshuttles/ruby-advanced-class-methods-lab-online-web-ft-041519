a5d4d3ad363e0241274c5f7414fc426d23a482fe
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-26 15:35:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,9 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  
   def self.all
     @@all
   end
@@ -10,4 +12,37 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new 
+    song.save 
+    song 
+  end 
+  
+  def self.new_by_name(name)
+    song = Song.new 
+    song.name = name 
+    song 
+  end 
+  
+  def self.create_by_name(name)
+    song = Song.create  
+    song.name = name 
+    song 
+  end 
+  
+  def self.find_by_name(name)
+    @@all.find {|song| song.name == name} 
+  end 
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end 
+  
+  def self.alphabetical
+    self.sort 
+  end 
+  
+  def self.destroy_all 
+    @@all.clear
+  end 
 end
